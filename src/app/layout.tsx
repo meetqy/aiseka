35f3480fd40967c335e2111ac6d4ed653e184a31
meetqy <meetqy@icloud.com>
2024-10-16 13:59:26 +0800

    Refactor layout component to set the language to Chinese (Simplified)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="zh-CN">
       <body className={clsx("min-h-screen bg-content2 font-sans antialiased light")}>
         <NextTopLoader color="#115ae5" />
         <TRPCReactProvider>
